6abea320a971a16abe7bbf378c9f5eb1bbb3d81f
danbrus <danielerikverrier@gmail.com>
2025-12-20 17:37:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from astro_pi_orbit import ISS
+
+iss = ISS()
+print(iss.coordinates())
+
+# IERS2010 latitude +50.9931 N longitude -129.1483 E elevation 425686.5 m
+
+# IERS2010 latitude +50.1097 N longitude -123.3782 E elevation 425475.7 m
