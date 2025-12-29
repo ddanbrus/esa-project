e243ebb9f1b3cbf5594ecbf0d8dc68ba6326f62e
danbrus <danielerikverrier@gmail.com>
2025-12-29 17:07:29 +0200

    change 2
    - fixed problem where math.cos only accepts radians
    - accounted for altitude
    - now prints to result.txt and creates if does not exist

--- main.py
+++ main.py
@@ -4,65 +4,72 @@ from astro_pi_orbit import ISS
 import time
 import math
 
-DEGREES_IN_KM_EQUATOR = 111
-MINUTES_IN_KM_EQUATOR = DEGREES_IN_KM_EQUATOR / 60
-SECONDS_IN_KM_EQUATOR = MINUTES_IN_KM_EQUATOR / 60
+# (all in km)
+LONGITUDE_DEGREES_PER_KM_EQUATOR = 111
+LATITUDE_DEGREES_PER_KM = 111
+RADIUS_EARTH = 6371
+CIRCUMFERENCE_EARTH = 40075
 
-def difference_two_coordinates(coords1, coords2):
-    diffDegrees = coords2[0]  - coords1[0]
-    diffMinutes = coords2[1] - coords1[1]
-    diffSeconds = coords2[2] - coords1[2]
-    
-    return [diffDegrees, diffMinutes, diffSeconds]    
+# converts degrees, minutes, seconds to degrees as a float
+def dms_to_d(dms):
+    sign, d, m, s = dms[0], dms[1], dms[2], dms[3]
+    return sign * (d + m/60 + s/3600)
 
-# need latitude because distance of degrees varies depending on the latitude
-# (eg. at equator, 1 degree is 111km; at pole, 1 degree is basically 0)
-# Formula is:
-# distance in km at certain latitude = km at equator * cos(degrees of latitude)
-def convert_longitude_to_km_from_origin(delta_longitude, latitude):
-    degreesLong, minutesLong, secondsLong = delta_longitude[0], delta_longitude[1], delta_longitude[2]
-    degreesLat, minutesLat, secondsLat = latitude[0], latitude[1], latitude[2]
-    
-    km_per_degree = DEGREES_IN_KM_EQUATOR * math.cos(degreesLat)
-    km_per_minute = km_per_degree / 60
-    km_per_second = km_per_minute / 60
-    
-    km_from_origin = (degreesLong * km_per_degree) + (minutesLong * km_per_minute) + (secondsLong * km_per_second)
-    
-    return km_from_origin
-    
-# returns two lists (latitude, longitude) with the numbers appropriately negative
-# depending on side of planet
-def parse_coords(coords):
-    coords_lat_sd = coords.latitude.signed_dms()
-    coords_long_sd = coords.longitude.signed_dms()
-    latitude = [coords_lat_sd[1] * coords_lat_sd[0], coords_lat_sd[2] * coords_lat_sd[0], coords_lat_sd[3]* coords_lat_sd[0]]
-    longitude = [coords_long_sd[1] * coords_long_sd[0], coords_long_sd[2]* coords_long_sd[0], coords_long_sd[3] * coords_long_sd[0]]
-    
-    return latitude, longitude
+# converts degrees of latitude into km which requires the latitude because longitude
+# varies based on the latitude (equator -> 111km, poles -> ~0km)
+def dlong_to_km(d_long, d_lat):
+    km_per_degree = LONGITUDE_DEGREES_PER_KM_EQUATOR * math.cos(math.radians(d_lat))
+    return d_long * km_per_degree
+
+# the distance 1 degree of latitude represents stays more or less constant at around 111km
+def dlat_to_km(d_lat):
+    return d_lat * LATITUDE_DEGREES_PER_KM 
+
+def difference_degrees(d1, d2):
+    diff = abs(d2 - d1)
+    diff = min(diff, 360 - diff)
+    return diff
 
 ####################################
 
+
 t = 10
-
 iss = ISS()
-coords1 = iss.coordinates()
+
+c1 = iss.coordinates()
+c1_altitude = c1.elevation.km
+c1_long = dms_to_d( c1.longitude.signed_dms() ) # in degrees as a decimal
+c1_lat = dms_to_d( c1.latitude.signed_dms() )   # in degrees as a decimal
+
 time.sleep(t)
-coords2 = iss.coordinates()
 
-coords1latitude, coords1longitude = parse_coords(coords1)
-coords2latitude, coords2longitude = parse_coords(coords2)
+c2 = iss.coordinates()
+c2_altitude = c2.elevation.km
+c2_long = dms_to_d( c2.longitude.signed_dms() ) # in degrees as a decimal
+c2_lat = dms_to_d( c2.latitude.signed_dms() )   # in degrees as a decimal
 
-print("Inital longitude: ", coords1longitude)
-print("Final longitude: ", coords2longitude)
+displacement_lon = difference_degrees(c1_long, c2_long)
+displacement_lat = difference_degrees(c1_lat, c2_lat)
+average_lat = (c1_lat + c2_lat) / 2
+average_altitude = (c1_altitude + c2_altitude) / 2
 
-diff = difference_two_coordinates(coords1longitude, coords2longitude)
-print("Difference is ", diff)
+# convert to kilometres
+displacement_long_km = dlong_to_km(displacement_lon, average_lat)
+displacement_lat_km = dlat_to_km(displacement_lat)
 
-km_diff = convert_longitude_to_km_from_origin(diff, coords1latitude)
-print("In km that is ", km_diff)
+# total displacement = sqrt( (longitude displacement)^2 + (latitude displacement)^2 )
+total_displacement_surface_km = math.sqrt(displacement_long_km**2 + displacement_lat_km**2)
 
-km_per_second = km_diff / t
-print("So that is ", km_per_second, " kilometres per second")
+# get theta as an angle using the circumference of the earth
+theta = (total_displacement_surface_km / CIRCUMFERENCE_EARTH) * 360
+# using the formula to find the length of an arc (theta/360 * 2PiR), we find the distance
+# travelled accounting for altitude
+total_displacement_in_altitude_km = (theta/360) * 2*math.pi*(RADIUS_EARTH + average_altitude)
 
 
+velocity_kms = total_displacement_in_altitude_km  / t
+print("velocity is ", velocity_kms, " km/s")
+
+
+with open("result.txt", "w") as f:
+    f.write(f"{velocity_kms:.5g}\n")
